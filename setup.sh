f5cc3afc135f1178b3014c05669ef829989a4ee4
Dustin Essington <aetaric@gmail.com>
2017-07-08 00:14:37 -0400

    Add RVM steps to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,18 @@
 # install git
 apt-get install git
 
+# install ruby via rvm
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.2.2
+rvm use 2.2.2 --default
+
+# Set rvm to load ruby 2.2.2 on bash startup
+echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
+echo "rvm use 2.2.2" >> ~/.bashrc
+
 # pull down the source and switch to it's directory
 git clone https://github.com/aetaric/lightbot
 cd lightbot

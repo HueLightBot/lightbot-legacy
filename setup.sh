2abcfc7a4ab31e45041927fc196ef106472c8c6a
Dustin Essington <aetaric@gmail.com>
2017-07-08 01:10:59 -0400

    Changes after testing!

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # install git
-apt-get install git
+apt-get update
+apt-get install -y git
 
 # install ruby via rvm
-apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev libcurl4-openssl-dev
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm

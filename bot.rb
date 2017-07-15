436f9f5a1bc052c57ab7bc8c858838e75760e565
Dustin Essington <aetaric@ecs-server.net>
2017-07-15 13:27:50 -0400

    Added basic logging

--- bot.rb
+++ bot.rb
@@ -18,22 +18,31 @@ require 'hue'
 # require YAML for loading the config
 require 'yaml'
 
+# require logger to log info about bot actions
+require 'logger'
+
+# start up logger
+$lightbot_logger = Logger.new('lightbot.log')
+
 # load custom cinch plugins
 Dir["./plugins/*.rb"].each {|file| load file}
 
 # Create hue instance.
+$lightbot_logger.info "Starting Hue client"
 $hue_client = Hue::Client.new
 
 # Load Twitch Plugin
 plugins = [constantize("Twitch")]
 
 # Load Config from YAML
+$lightbot_logger.info "Loading Config"
 $config = YAML.load_file('config.yaml')
 
 # Join Channel
 channels = [$config["bot"]["channel"]]
 
 # Configure Bot instance
+$lightbot_logger.info "Configuring Bot from config file"
 @bot = Cinch::Bot.new do
   configure do |c|
     c.nick = $config["bot"]["nick"]
@@ -51,4 +60,5 @@ end
 $plugin_list = Cinch::PluginList.new @bot
 
 # Start bot
+$lightbot_logger.info "Starting Lightbot"
 @bot.start
--- plugins/twitch.rb
+++ plugins/twitch.rb
@@ -15,21 +15,27 @@ class Twitch
   match /on/, method: :on
 
   def lights(m, color)
+    $lightbot_logger.info "Detected RGB hex color #{color}"
     if m.tags["bits"].to_i >= $config["bot"]["cheer_floor"].to_i
+      $lightbot_logger.info "Cheer is higher than configured cheer floor. Setting color."
       temp_color = "#" + color.to_s
       set_color m, temp_color
     end
   end
 
   def mod_color(m, color)
+    $lightbot_logger.info "Detected !setlights command!"
     if mod?(m)
+      $lightbot_logger.info "Lights are being set to #{color} by command!"
       temp_color = "#" + color.to_s
       set_color m, temp_color
     end
   end
 
   def off(m)
+    $lightbot_logger.info "Detected !off command!"
     if m.tags["bits"].to_i >= $config["bot"]["off_floor"].to_i
+      $lightbot_logger.info "Cheer is higher than configured cheer floor for turning the lights off."
       $hue_client.group($config["bot"]["hue_group"]).lights.each do |light|
         light.off!
       end
@@ -37,7 +43,9 @@ class Twitch
   end
 
   def on(m)
+    $lightbot_logger.info "Detected !on command!"
     if m.tags["bits"].to_i >= $config["bot"]["on_floor"].to_i
+      $lightbot_logger.info "Cheer is higher than configured cheer floor for turning the lights on."
       $hue_client.group($config["bot"]["hue_group"]).lights.each do |light|
         light.on!
       end
@@ -46,8 +54,10 @@ class Twitch
 
   def subs(m)
     if m.tags["msg-id"] == "resub"
+      $lightbot_logger.info "Sub/resub!"
       if /#([0-9a-fA-F]{6})/.match(m.message)
         color = /#([0-9a-fA-F]{6})/.match(m.message)[0]
+        $lightbot_logger.info "Resub message includes RGB hex code. Setting lights to color #{color}"
         set_color m, color
       end
     end
@@ -66,7 +76,9 @@ class Twitch
       light.brightness = @bri
     end
 
+    $lightbot_logger.info "Opening color text file."
     file = File.open "current_color.txt", 'w'
+    $lightbot_logger.info "Writing color value to file."
     file.puts color
     file.flush
     file.close
